42607fd67aada88ff4d345df28ded685abe2c9c6
JBrendon15 <110281109+JBrendon15@users.noreply.github.com>
2022-10-27 17:33:14 -0500

    added put function to keep previous text on page, working on the service worker and manifest

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -3,19 +3,32 @@ import { openDB } from 'idb';
 const initdb = async () =>
   openDB('jate', 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
+      if (db.objectStoreNames.contains('jateStorage')) {
         console.log('jate database already exists');
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      db.createObjectStore('jateStorage', { keyPath: 'id', autoIncrement: true });
       console.log('jate database created');
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+    const jateDB = await openDB('jate', 1);
+    const tx = jateDB.transaction('jateStorage', 'readwrite');
+    const store = tx.objectStore('jateStorage');
+    const request = store.put({ jate: content });
+    const result = await request;
+    console.log('🚀 - data saved to the database', result);
+  }
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
-
+export const getDb = async () => {
+  const jateDB = await openDB('jate', 1);
+  const tx = jateDB.transaction('jateStorage', 'readonly');
+  const store = tx.objectStore('jateStorage');
+  const request = store.getAll();
+  const result = await request;
+  console.log('result.value', result);
+}
 initdb();
